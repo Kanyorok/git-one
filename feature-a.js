c5dd4bc857e6e28401c67b0a2ef64fc96046aac6
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2023-02-23 21:23:52 +0300

    The files for HTML

---
+++ feature-a.js
@@ -0,0 +1,1 @@
+console.log('temporary branches tests')
---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("I am actually testing if this can work!!")
+console.log("The branch refused to be killed")
+function test(name = 'work'){
+  console.log(name);
+}
+
+test();
